d957fd3adc2a2b969f60bae1e3d878567431c3ef
SayedSarwary <sarwary@pdx.edu>
2022-03-04 19:20:51 -0800

    files

--- src/Member.cpp
+++ src/Member.cpp
@@ -1,42 +1,48 @@
 #include "Member.h"
 
-Member::Member() {
+Member::Member()
+{
 }
 
-Member::Member(const string& u_name, const int id, const string& st, const string& city, const string& state, const int zip) 
-    :Address(u_name, id, st, city, state, zip) {
-
+Member::Member(const string &u_name, const int id, const string &st, const string &city, const string &state, const int zip)
+    : Address(u_name, id, st, city, state, zip)
+{
 }
 
-Member::Member(Member& to_copy) 
-    :Address(to_copy) {
-
+Member::Member(Member &to_copy)
+    : Address(to_copy)
+{
 }
 
-Member::~Member() {
-
+Member::~Member()
+{
 }
 
-bool Member::operator<(const Member& compare) {
+bool Member::operator<(const Member &compare)
+{
     return Information::operator<(compare.id);
 }
-bool Member::operator<=(const Member& compare) {
+bool Member::operator<=(const Member &compare)
+{
     return Information::operator<(compare.id);
 }
-bool Member::operator>(const Member& compare) {
+bool Member::operator>(const Member &compare)
+{
     return Information::operator<(compare.id);
 }
-bool Member::operator>=(const Member& compare) {
+bool Member::operator>=(const Member &compare)
+{
     return Information::operator<(compare.id);
 }
-bool Member::operator!=(const Member& compare) {
+bool Member::operator!=(const Member &compare)
+{
     return Information::operator<(compare.id);
 }
-bool Member::operator==(const Member& compare) {
+bool Member::operator==(const Member &compare)
+{
     return Information::operator<(compare.id);
 }
 
-
-Service& Member::getService(const int id) {
-
+Service &Member::getService(const int id)
+{
 }
