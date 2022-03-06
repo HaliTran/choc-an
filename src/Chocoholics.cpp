20e6367ac9a3371d53f92258ddf7975518fb7b19
natnaely91 <natnaely91@gmail.com>
2022-03-05 16:21:48 -0800

    hello

--- src/Chocoholics.cpp
+++ src/Chocoholics.cpp
@@ -1,10 +1,13 @@
 #include "Chocoholics.h"
 
-
 int Chocoholics::getMember(const int id, Member* mem) {
     mem = new Member("Black", 876543210, "123th", "portland", "OR", 97000);
     return 0;
 
     //0 = does exist
     //1 = does not exist
+}
+
+int Chocoholics::getProvider(const int id, Provider* pro) {
+    pro = new Provider("Star", 111111111, "321th", "Portland", "OR", 97111);
 }
--- src/Chocoholics.h
+++ src/Chocoholics.h
@@ -1,7 +1,9 @@
 #pragma once
+#include "Provider.h"
 #include "Member.h"
 
 class Chocoholics {
     public:
         int getMember(const int id, Member* mem);
+        int getProvider(const int id, Provider* pro);
 };
