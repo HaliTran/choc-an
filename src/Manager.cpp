2762e3950e8b32f4883cc8c1628d59a9843f25e0
Hali Tran <78198010+HaliTran@users.noreply.github.com>
2022-03-05 17:03:38 -0800

    delete address and created menu for manager

--- src/Manager.cpp
+++ src/Manager.cpp
@@ -1,7 +1,28 @@
 #include "Manager.h"
 
-//Manager constructor and destructor
+//manager constructor and destructor
 Manager::Manager(){}
 
 Manager::~Manager(){}
+
+//display menu option for user
+void Manager::menu(){
+    char input;
+    do
+    {
+        cout<<"1 = Add member to the ChocAn Database"<<endl;
+        cout<<"2 = Add provider to the ChocAn Database"<<endl;
+        cout<<"3 = Delete member from the ChocAn Database"<<endl;
+        cout <<"4 = Delete provider from the ChocAn Database" << endl;
+        cout <<"5 = Update member information" << endl;
+        cout <<"6 = Update provider information" << endl;
+        cout <<"7 = Generate report about a member/provider" << endl;
+        cout <<"0 = Exit" << endl;
+        cout<<"Enter: ";
+        cin>>input;
+        cin.ignore(100, '\n');
+
+    } while (input != '0');
+    
+}
 
