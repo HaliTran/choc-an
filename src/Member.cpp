8321898147f5be08ee244cf47849a02f5d4e2751
Ruskidre1 <andrey8@pdx.edu>
2022-03-05 18:10:01 -0800

    yeo

--- src/Member.cpp
+++ src/Member.cpp
@@ -62,7 +62,6 @@ bool Member::generateReport()
         return 1;
     }
     //else open
-
     myfile << "Member Report" << endl;
     myfile << getUserName() << endl;    //Member name (25 characters). 
     myfile << getId() << endl;          //Member number (9 digits).
@@ -70,44 +69,16 @@ bool Member::generateReport()
     myfile << getCity() << endl;        //Member city (14 characters). 
     myfile << getState() << endl;       //Member state (2 letters). 
     myfile << getZipCode() << endl;     //Member zip code (5 digits).
-    
-    vector<Service> consult;
         
     for (auto i = consult.begin(); i != consult.end(); ++i)
     {
         //Date of service (MM-DD-YYYY).
-        myfile << *i.getMonth() << *i.getDay() << *i.getYear() << *i.getCompTime() << endl;
-        myfile << *i.getUserName() << endl;     //Provider name (25 characters)
-        myfile << *i.getServiceName() << endl;   //Service name (20 characters). 
+        myfile << i->getMonth() << i->getDay() << i->getYear() << i->getCompTime() << endl;
+        myfile << i->getUserName() << endl;     //Provider name (25 characters)
+        myfile << i->getServiceName() << endl;   //Service name (20 characters). 
     }
 
-    //Date of service (MM-DD-YYYY). 
-    //Provider name (25 characters). 
-    //Service name (20 characters). 
-
-    //to get member name go to member->addy->information
-
-   //For each service provided, the following details are required: 
-
-    // Address class contain
-    /*
-        string street;
-        string city;
-        string state;
-        int zip_code;
-
-    //Member class has service vector contains
-        string service_name;
-        int service_code;
-        double fee;
-        string comment;
-    // Date includes
-        int month;
-        int day;
-        int year;
-        time_t comp_time;
-
-    */
+    
     myfile.close(); 
 
 
